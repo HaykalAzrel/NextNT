47a3557a44e11e4b50073e2896241132356c19b6
haykalazrel@gmail.com <haykalazrel@gmail.com>
2025-11-01 14:33:59 +0700

    nt

--- src/components/Tambah.tsx
+++ src/components/Tambah.tsx
@@ -1,7 +1,21 @@
 const FormTambah = () => {
   return (
-    <div>
-        <h1>Form Tambah Barang</h1>
+    <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-white shadow-md rounded pc-8 pt-6 mb-4 w-full max-w-md">
+            <h2 className="text-3x1 font-bold mb-6 text-center text-white">
+                <span className="bg-gradient-to--r text-transparent from-yellow-500 to-yellow-700 bg-clip-text">
+                    Tambah barang
+                </span>
+            </h2>
+            <form>
+                <div className="mb-6">
+                    <label htmlFor="Nama Barang"
+                    className="block text-gray-600 text-sm font-bold mb-2">
+                        Nama barang
+                    </label>
+                </div>
+            </form>
+        </div>
     </div>
     )
 }
